ab59525f1fe9a5db6d86b68efa4358204e2c8ae8
niklasdschnr <deschner.niklas@t-online.de>
2022-11-06 16:57:21 +0100

    my first simple event handler aus MATSim Class Seminar 3

--- src/main/java/org/matsim/analysis/SimpleAnalysis.java
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -1,15 +1,33 @@
 package org.matsim.analysis;
 
+import org.apache.commons.csv.CSVFormat;
 import org.matsim.core.events.EventsUtils;
+
+import  java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SimpleAnalysis {
 
+    private static final String eventsFile = "/Users/niklas/Documents/Masterarbeit_lokal/matsim-serengeti-park/scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz";
+    private static final String outFile = "/Users/niklas/Documents/Masterarbeit_lokal/matsim-serengeti-park/scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_link_count.csv";
+
     public static void main(String[] args){
-
-        var handler = new SimplePersonEventHandler();
         var manager = EventsUtils.createEventsManager();
-        manager.addHandler(handler);
+        var personHandler = new SimplePersonEventHandler();
+        var linkHandler = new LinkEventHandler();
+        manager.addHandler(personHandler);
+        manager.addHandler(linkHandler);
 
-        EventsUtils.readEvents(manager, "/Users/niklas/Documents/Masterarbeit_lokal/matsim-serengeti-park/scenarios/serengeti-park-v1.0/output/output-serengeti-park-v1.0-run1/serengeti-park-v1.0-run1.output_events.xml.gz");
+        EventsUtils.readEvents(manager, eventsFile);
+        var volumes = linkHandler.getVolumes();
+        try (var writer = Files.newBufferedWriter(Paths.get(outFile)); var printer = CSVFormat.DEFAULT.withDelimiter(';').withHeader("Hour", "Value").print(writer)){
+            for(var volume :volumes.entrySet()){
+                printer.printRecord(volume.getKey(), volume.getValue());
+                printer.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/matsim/analysis/SimplePersonEventHandler.java
+++ src/main/java/org/matsim/analysis/SimplePersonEventHandler.java
@@ -1,20 +1,39 @@
 package org.matsim.analysis;
 
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
 import org.matsim.api.core.v01.events.PersonDepartureEvent;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
+import org.matsim.api.core.v01.population.Person;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimplePersonEventHandler implements PersonDepartureEventHandler, PersonArrivalEventHandler {
+
+    //define a map variable to use it everywhere, map with id mapping to some value
+    private final Map<Id<Person>, Double> persontoDepartureTime = new HashMap<>();
+    //final: gurantee that the hashmap won't change
+
     @Override
     public void handleEvent(PersonArrivalEvent personArrivalEvent) {
-        System.out.println("Arrival: " + personArrivalEvent.getTime() + ": " + personArrivalEvent.getPersonId());
+        //the event handler will get called whenever the event occurs, not in the order the code is written here
+        var arrivalTime = personArrivalEvent.getTime();
+        var departureTime=persontoDepartureTime.get(personArrivalEvent.getPersonId());
+        var travelTime=arrivalTime-departureTime;
+
+        System.out.println("Person: " + personArrivalEvent.getPersonId() + "travelled   " + travelTime + " sec  (" + travelTime/60 + "min)");
+        //System.out.println("Arrival: " + personArrivalEvent.getTime() + ": " + personArrivalEvent.getPersonId());
     }
 
 
     @Override
     public void handleEvent(PersonDepartureEvent personDepartureEvent) {
-        System.out.println("Departure: " + personDepartureEvent.getTime() + ": " + personDepartureEvent.getPersonId());
+        var departureTime = personDepartureEvent.getTime();
+        var personId =personDepartureEvent.getPersonId();
+        persontoDepartureTime.put(personId, departureTime);
+        //System.out.println("Departure: " + personDepartureEvent.getTime() + ": " + personDepartureEvent.getPersonId());
 
     }
 }
